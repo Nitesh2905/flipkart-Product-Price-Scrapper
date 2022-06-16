2cc8bb6bb15b02fc9213d7f846ef4c60b3b7d4fe
Nitesh2905 <nkyadav2905@gmail.com>
2022-06-16 16:07:53 +0530

    used sendmail to send mails

--- parser.js
+++ parser.js
@@ -1,4 +1,5 @@
 const nightmare = require("nightmare")();
+const sendmail = require("sendmail")();
 // https://www.flipkart.com/boat-airdopes-131-upto-15-hours-playback-13mm-drivers-iwp-technology-bluetooth-headset/p/itmf77ad6e8eecf1 url from flipkart
 //("a-price-whole"); class of price
 
@@ -9,22 +10,50 @@ const url = args[0];
 const minPrice = args[1];
 
 async function checkPrice() {
-  const priceString = await nightmare
-    .goto(url) //url of the product = https://www.amazon.in/Airdopes-141-Playtime-Resistance-Bluetooth/dp/B09N3ZNHTY
-    .wait("._30jeq3._16Jk6d")
-    .evaluate(() => document.querySelector("._30jeq3._16Jk6d").innerText)
-    .end();
+  try {
+    const priceString = await nightmare
+      .goto(url) //url of the product = https://www.amazon.in/Airdopes-141-Playtime-Resistance-Bluetooth/dp/B09N3ZNHTY
+      .wait("._30jeq3._16Jk6d")
+      .evaluate(() => document.querySelector("._30jeq3._16Jk6d").innerText)
+      .end();
 
-  let price = priceString.replaceAll("₹", "").replaceAll(",", "");
+    let price = priceString.replaceAll("₹", "").replaceAll(",", "");
 
-  const priceNumber = parseFloat(price);
+    const priceNumber = parseFloat(price);
 
-  console.log(priceNumber);
-  if (priceNumber < minPrice) {
-    console.log("Current Price is cheap");
-  } else {
-    console.log("Its expensive right now!");
+    console.log(priceNumber);
+    if (priceNumber < minPrice) {
+      sendMail(
+        `Price Drop Alert‼️ @${priceNumber}`,
+        `Hurry up the price of Product at ${url} dropped to <strong>Rs ${priceNumber}</strong>,<br> 
+      Visit to buy fast!<br> 
+      Thanks.`
+      );
+      console.log("Current Price is cheap");
+    } else {
+      console.log("Its expensive right now!");
+    }
+  } catch (err) {
+    sendMail("Error!", "Flipkart price checker throw an error");
+    throw err;
   }
+}
+
+//function send email to the person
+
+function sendMail(subject, body) {
+  sendmail(
+    {
+      from: "no-reply@Nitesh2905.com",
+      to: "navego4828@mahazai.com", //enter your email here
+      subject: subject,
+      html: body,
+    },
+    function (err, reply) {
+      console.log(err && err.stack);
+      console.dir(reply);
+    }
+  );
 }
 
 checkPrice();
